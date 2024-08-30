6c23bd0f7a8e2bba88ab5f4835de6243ff32f3f6
Facug03 <facundogonzales1550@gmail.com>
2024-08-30 19:39:58 -0300

    first commit

---
+++ api/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+
+import { HealthModule } from "@/src/health/health.module";
+import { LoggerModule } from "@/src/logger/logger.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    LoggerModule,
+    HealthModule,
+  ],
+})
+export class AppModule {}
