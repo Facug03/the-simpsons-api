65c1eba73dad3fc097bd68a098d116da53ec878d
Facug03 <facundogonzales1550@gmail.com>
2024-09-02 12:18:15 -0300

    feat: install and config prisma with postgresql

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,8 +3,9 @@ import { ConfigModule } from '@nestjs/config'
 
 import { HealthModule } from '@/src/health/health.module'
 import { LoggerModule } from '@/src/logger/logger.module'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true }), LoggerModule, HealthModule]
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true }), LoggerModule, HealthModule, DatabaseModule]
 })
 export class AppModule {}
