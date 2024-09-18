bd52f7fde2716c5597c36f3d44c7718cd4fcb6cb
Facug03 <facundogonzales1550@gmail.com>
2024-09-18 10:44:52 -0300

    feat: cache first page results

--- api/src/modules/character/character.service.ts
+++ api/src/modules/character/character.service.ts
@@ -35,30 +35,55 @@ export class CharacterService {
   ): Promise<
     PaginationResponse<Omit<Character, 'first_appearance_ep_id' | 'description' | 'first_appearance_sh_id'>[]>
   > {
-    const cacheKey = 'character-count'
-    let count = await this.cacheManager.get<number>(cacheKey)
+    const cacheCountKey = 'character-count'
+    const cacheFirstPageKey = 'character-first-page'
+
+    let count = await this.cacheManager.get<number>(cacheCountKey)
+    let results =
+      page === 1
+        ? await this.cacheManager.get<
+            Omit<Character, 'first_appearance_ep_id' | 'description' | 'first_appearance_sh_id'>[]
+          >(cacheFirstPageKey)
+        : null
 
     if (!count) {
       count = await this.databaseService.character.count()
 
-      await this.cacheManager.set(cacheKey, count, 60 * 60 * 24 * 1000)
+      await this.cacheManager.set(cacheCountKey, count, 60 * 60 * 24 * 1000)
     }
 
-    const results = await this.databaseService.character.findMany({
-      take: 20,
-      skip: (page - 1) * 20,
-      select: {
-        id: true,
-        age: true,
-        birthdate: true,
-        gender: true,
-        name: true,
-        occupation: true,
-        portrait_path: true,
-        phrases: true,
-        status: true
+    const pages = Math.ceil(count / 20)
+
+    if (page > pages) {
+      return {
+        count,
+        next: null,
+        prev: null,
+        pages,
+        results: []
       }
-    })
+    }
+
+    if (!results) {
+      results = await this.databaseService.character.findMany({
+        take: 20,
+        skip: (page - 1) * 20,
+        select: {
+          id: true,
+          age: true,
+          birthdate: true,
+          gender: true,
+          name: true,
+          occupation: true,
+          portrait_path: true,
+          phrases: true,
+          status: true
+        }
+      })
+
+      if (page === 1) await this.cacheManager.set(cacheFirstPageKey, results, 60 * 60 * 24 * 1000)
+    }
+
     const APP_URL = this.configService.get<string>('APP_URL')
 
     return {
